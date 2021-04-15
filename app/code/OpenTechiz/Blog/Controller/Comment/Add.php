ccb328bb707ca41899c1cc2e42bda4adc2004c22
hungpham <pvhung7792@gmail.com>
2021-04-15 13:21:15 +0700

    send email on comment

--- app/code/OpenTechiz/Blog/Controller/Comment/Add.php
+++ app/code/OpenTechiz/Blog/Controller/Comment/Add.php
@@ -2,11 +2,17 @@
 namespace OpenTechiz\Blog\Controller\Comment;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Area;
+use Magento\Store\Model\Store;
+use Mageplaza\Smtp\Helper\Data as SmtpData;
 use OpenTechiz\Blog\Api\CommentRepositoryInterface;
 use OpenTechiz\Blog\Api\Data\CommentInterface;
 use Magento\Framework\Stdlib\DateTime\DateTime;
 use Magento\Framework\Controller\Result\JsonFactory;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\StoreManager;
 
 class Add extends Action
 {
@@ -18,17 +24,29 @@ class Add extends Action
 
     protected $_resultFactory;
 
+    protected $_transportBuilder;
+
+    protected $_scopeConfig;
+
+    protected $_storeManager;
+
     public function __construct(\Magento\Framework\App\Action\Context $context,
                                 CommentRepositoryInterface $commentRepository,
                                 DateTime $dateTime,
                                 CommentInterface $commentInterface,
-                                JsonFactory $jsonFactory
+                                JsonFactory $jsonFactory,
+                                TransportBuilder $transportBuilder,
+                                ScopeConfigInterface $scopeConfig,
+                                StoreManager $storeManager
     )
     {
         $this->_dateTime = $dateTime;
         $this->_commentRepository = $commentRepository;
         $this->_commentInterface = $commentInterface;
         $this->_resultFactory = $jsonFactory;
+        $this->_transportBuilder = $transportBuilder;
+        $this->_scopeConfig = $scopeConfig;
+        $this->_storeManager = $storeManager;
         parent::__construct($context);
     }
 
@@ -41,9 +59,6 @@ class Add extends Action
         $comment->setUserId($data['user_id']);
         $comment->setContent($data['content']);
         $comment->setPostId($data['post_id']);
-        /*$comment->setUserId(1);
-        $comment->setContent('asdhasjhdg');
-        $comment->setPostId(24);*/
         $comment->setIsActive(0);
         $comment->setCreationTime($this->_dateTime->gmtDate());
 
@@ -53,17 +68,31 @@ class Add extends Action
             $this->_commentRepository->save($comment);
             $message = "Your comment have been add success, plz wait for admin to approve";
             $jsonResultFactory->setData(['result'=>'success','message'=>$message]);
-            $this->_eventManager->dispatch("customer_comment_success", ['email' => $data['email']]);
+            /*$this->_eventManager->dispatch("customer_comment_success", ['email' => $data['email']]);*/
         }catch (\Exception $e){
-//            $message = "Add comment fail";
             $message = $e->getMessage();
-            $jsonResultFactory->setData(['result'=>'error','message'=>$message]);
+            $jsonResultFactory->setData(['result'=>'error','message'=>'Add comment fail '.$message]);
         }
 
-        /*$resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+        $transport = $this->_transportBuilder->setTemplateIdentifier($this->_scopeConfig->getValue('blog/general/template',$storeScope))
+            ->setTemplateOptions(
+                [
+                    'area'=> \Magento\Backend\App\Area\FrontNameResolver::AREA_CODE,
+                    'store'=> $this->_storeManager->getStore()->getId()
+                ]
+            )
+            ->setTemplateVars(['name'=>$data['first_name']])
+            ->setFrom($this->_scopeConfig->getValue('blog/general/sender_email',$storeScope))
+            ->addTo(['pvhung7792@gmail.com'])
+            ->setReplyTo('pvhung7792@gmail.com')
+            ->getTransport();
+        $transport->sendMessage();
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
 
         $resultRedirect->setUrl($this->_redirect->getRefererUrl());
-        return $resultRedirect;*/
-        return $jsonResultFactory;
+        return $resultRedirect;
+//        return $jsonResultFactory;
     }
 }
+
